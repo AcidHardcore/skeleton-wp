dfbce4ea92fe885af7148cf57d51f9f16c04df5c
AcidHardcore <vitcore@mail.ru>
2017-06-06 23:14:26 +0300

    added preconnect function to speed up a fonts loading

--- inc/fonts.php
+++ inc/fonts.php
@@ -33,3 +33,24 @@ $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
 
 return esc_url_raw( $fonts_url );
 }
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function understrap_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'understrap-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'understrap_resource_hints', 10, 2 );
