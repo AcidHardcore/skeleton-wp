2276b973100cfde4dbf893f3be7903ddcd87dfe3
AcidHardcore <vitcore@mail.ru>
2017-06-06 23:14:26 +0300

    added preconnect function to speed up a fonts loading

--- inc/fonts.php
+++ inc/fonts.php
@@ -25,7 +25,7 @@ if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
 
 $query_args = array(
 'family' => urlencode( implode( '|', $font_families ) ),
-'subset' => urlencode( 'latin' ),
+'subset' => urlencode( 'latin,cyrillic' ),
 );
 
 $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
@@ -33,3 +33,24 @@ $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
 
 return esc_url_raw( $fonts_url );
 }
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function understrap_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'understrap-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'understrap_resource_hints', 10, 2 );
