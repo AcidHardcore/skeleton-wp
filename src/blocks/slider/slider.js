f6d8d44a51a516e82ae85b753bdceb8375120678
Vitalii Ivanichko <vitcore@mail.ru>
2017-10-27 13:09:00 +0300

    fixed slider to swiper

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,15 +1,25 @@
 (function () {
-    const elem = document.querySelector('.slider__container');
-    if (elem) {
-        const flkty = new Flickity(elem, {
-            contain: true,
-            wrapAround: true,
-            setGallerySize: false,
-            imagesLoaded: true
-            // percentPosition:true,
-            // cellSelector: '.slider-cell'
-        });
-    }
+    let mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+
+        // And if we need scrollbar
+        scrollbar{
+            el: '.swiper-scrollbar',
+        },
+    })
 })();
 
 
