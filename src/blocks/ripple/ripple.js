666eec5e8c328e38deb9ed3a4f05d9ef935f4e16
vivanichko <vitcore@mail.ru>
2017-11-16 10:12:25 +0300

    add ripple hover effect

--- src/blocks/ripple/ripple.js
+++ src/blocks/ripple/ripple.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
     /**
      * Ripple effect mechanism
      */
-    $('body').on('click', '.ripple-effect, [class*="--ripple-effect"]', function (e) {
+    $('body').on('click', '.ripple-effect-click, [class*="--ripple-effect-click"]', function (e) {
         // Ignore default behavior
         e.preventDefault();
 
@@ -14,21 +14,21 @@ jQuery(document).ready(function ($) {
 
         // Get custom color for ripple effect
         var color = the_dom.attr('data-ripple-color');
-        if (typeof color == 'undefined') {
-            var color = 'rgba( 255, 255, 255, 0.2 )';
+        if (typeof color === 'undefined') {
+             color = 'rgba( 255, 255, 255, 0.2 )';
         }
 
         // Get ripple radius
         var radius = the_dom.attr('data-ripple-wrap-radius');
-        if (typeof radius == 'undefined') {
-            var radius = 0;
+        if (typeof radius === 'undefined') {
+             radius = 0;
         }
 
         // Get the clicked element and the click positions
-        if (typeof limit == 'undefined') {
+        if (typeof limit === 'undefined') {
             var the_dom_limit = the_dom;
         } else {
-            var the_dom_limit = the_dom.closest(limit);
+             the_dom_limit = the_dom.closest(limit);
         }
 
         var the_dom_offset = the_dom_limit.offset();
@@ -40,7 +40,7 @@ jQuery(document).ready(function ($) {
         var the_dom_height = the_dom_limit.outerHeight();
 
         // Draw the ripple effect wrap
-        var ripple_effect_wrap = $('<span class="ripple-effect-wrap"></span>');
+        var ripple_effect_wrap = $('<span class="ripple-effect-click-wrap"></span>');
         ripple_effect_wrap.css({
             'width': the_dom_width,
             'height': the_dom_height,
@@ -57,7 +57,7 @@ jQuery(document).ready(function ($) {
         // Adding custom class, it is sometimes needed for customization
         var ripple_effect_wrap_class = the_dom.attr('data-ripple-wrap-class');
 
-        if (typeof ripple_effect_wrap_class != 'undefined') {
+        if (typeof ripple_effect_wrap_class !== 'undefined') {
             ripple_effect_wrap.addClass(ripple_effect_wrap_class);
         }
 
@@ -67,17 +67,17 @@ jQuery(document).ready(function ($) {
         // Determine the position of the click relative to the clicked element
         var click_x_ripple = click_x - the_dom_offset.left;
         var click_y_ripple = click_y - the_dom_offset.top;
-        var circular_width = 1000;
+        var circular = 1000;
 
         // Draw the ripple effect
         var ripple = $('<span class="ripple"></span>');
         ripple.css({
-            'width': circular_width,
-            'height': circular_width,
+            'width': circular,
+            'height': circular,
             'background': color,
             'position': 'absolute',
-            'top': click_y_ripple - ( circular_width / 2 ),
-            'left': click_x_ripple - ( circular_width / 2 ),
+            'top': click_y_ripple - ( circular / 2 ),
+            'left': click_x_ripple - ( circular / 2 ),
             'content': '',
             'background-clip': 'padding-box',
             '-webkit-border-radius': '50%',
@@ -89,7 +89,7 @@ jQuery(document).ready(function ($) {
             '-webkit-animation-fill-mode': 'both',
             'animation-fill-mode': 'both'
         });
-        $('.ripple-effect-wrap:last').append(ripple);
+        $('.ripple-effect-click-wrap:last').append(ripple);
 
         // Remove rippling component after half second
         setTimeout(function () {
@@ -104,30 +104,59 @@ jQuery(document).ready(function ($) {
         // Safari appears to ignore all the effect if the clicked link is not prevented using preventDefault()
         // To overcome this, preventDefault any clicked link
         // If this isn't hash, redirect to the given link
-        if (typeof href != 'undefined' && href.substring(0, 1) != '#') {
+        if (typeof href !== 'undefined' && href.substring(0, 1) !== '#') {
             setTimeout(function () {
                 window.location = href;
             }, 200);
         }
 
         // Ugly manual hack to fix input issue
-        if (!(the_dom.is('input') || the_dom.is('button'))) {
-        } else {
+        if (the_dom.is('input') || the_dom.is('button')) {
             var elClasses = the_dom.attr('class').split(' ');
             for (var index in elClasses) {
-                if (elClasses[index].match(/^\w--ripple-effect+$/)) {
+                if (elClasses[index].match(/^\wripple-effect-click+$/)) {
                     var className = elClasses[index];
                     break;
                 }
-                setTimeout(function () {
+                setTimeout(function (className) {
                     the_dom.removeClass(className);
-                    // the_dom.removeClass('ripple-effect');
+                    the_dom.removeClass('ripple-effect-click');
                     the_dom.trigger('click');
-                    // the_dom.addClass('ripple-effect');
+                    the_dom.addClass('ripple-effect-click');
                     the_dom.addClass(className);
                 }, 200);
             }
 
         }
-    });
+    })
+});
+
+jQuery(document).ready(function ($) {
+//animation button
+    effectTarget = $('.ripple-effect-hover');
+
+    const ripple_effect_span = $('<span class="ripple-effect-hover__spot"></span>');
+
+    ripple_effect_span.appendTo(effectTarget);
+
+    effectTarget.hover(
+        function (e) {
+            let parentOffset = $(this).offset();
+            let relX = e.pageX - parentOffset.left;
+            let relY = e.pageY - parentOffset.top;
+
+            $(this).find('.ripple-effect-hover__spot').css({
+                top: relY,
+                left: relX
+            })
+        },
+        function (e) {
+            let parentOffset = $(this).offset();
+            let relX = e.pageX - parentOffset.left;
+            let relY = e.pageY - parentOffset.top;
+            $(this).find('.ripple-effect-hover__spot').css({
+                top: relY,
+                left: relX
+            })
+        });
 });
