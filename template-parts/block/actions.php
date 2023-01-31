98d87610ef33b5f04ff7f6d5f957f464a6021feb
Vitalii Ivanychko <ivanichko@gmail.com>
2023-01-31 10:33:49 +0400

    updated the action template

--- template-parts/block/actions.php
+++ template-parts/block/actions.php
@@ -2,14 +2,21 @@
  * @var array $args
  */
 
-$ctas = $args['ctas'] ?? [];
+$ctas    = $args['ctas'] ?? [];
+$classes = $args['classes'] ?? [];
+$align   = $args['align'] ?? '';
 
-if ( count($ctas)): ?>
-<div class="actions">
-	<?php foreach ( $ctas as $cta ): ?>
-		<?php get_template_part('template-parts/block/cta', null, [
-			'cta' => $cta,
-		]); ?>
-	<?php endforeach; ?>
-</div>
+$classes = array_merge( [ 'actions' ], $classes );
+if ( $align ) {
+	$classes[] = $align;
+}
+
+if ( count( $ctas ) ): ?>
+	<div class="<?= join( ' ', $classes ) ?>">
+		<?php foreach ( $ctas as $cta ): ?>
+			<?php get_template_part( 'template-parts/block/cta', null, [
+				'cta' => $cta,
+			] ); ?>
+		<?php endforeach; ?>
+	</div>
 <?php endif;
