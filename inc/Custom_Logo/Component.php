d0e1a2390bd1dbf97e0587702a99ae6c44fb9f4b
Vitaly <vitaly@devphase.io>
2026-05-21 14:49:06 +0200

    updated function return type

--- inc/Custom_Logo/Component.php
+++ inc/Custom_Logo/Component.php
@@ -17,54 +17,59 @@ use function apply_filters;
  *
  * @link https://codex.wordpress.org/Theme_Logo
  */
-class Component implements Component_Interface {
+class Component implements Component_Interface
+{
 
-	/**
-	 * Gets the unique identifier for the theme component.
-	 *
-	 * @return string Component slug.
-	 */
-	public function get_slug() : string {
-		return 'custom_logo';
-	}
+  /**
+   * Gets the unique identifier for the theme component.
+   *
+   * @return string Component slug.
+   */
+  public function get_slug(): string
+  {
+    return 'custom_logo';
+  }
 
-	/**
-	 * Adds the action and filter hooks to integrate with WordPress.
-	 */
-	public function initialize() {
-		add_action( 'after_setup_theme', array( $this, 'action_add_custom_logo_support' ) );
-		add_filter( 'get_custom_logo', array( $this, 'change_logo_class') );
-	}
+  /**
+   * Adds the action and filter hooks to integrate with WordPress.
+   */
+  public function initialize(): void
+  {
+    add_action('after_setup_theme', array($this, 'action_add_custom_logo_support'));
+    add_filter('get_custom_logo', array($this, 'change_logo_class'));
+  }
 
-	/**
-	 * Adds support for the Custom Logo feature.
-	 */
-	public function action_add_custom_logo_support() {
-		add_theme_support(
-			'custom-logo',
-			apply_filters(
-				'skeleton_wp_custom_logo_args',
-				array(
-					'height'      => 250,
-					'width'       => 250,
-					'flex-width'  => false,
-					'flex-height' => false,
-				)
-			)
-		);
-	}
+  /**
+   * Adds support for the Custom Logo feature.
+   */
+  public function action_add_custom_logo_support(): void
+  {
+    add_theme_support(
+      'custom-logo',
+      apply_filters(
+        'skeleton_wp_custom_logo_args',
+        array(
+          'height' => 250,
+          'width' => 250,
+          'flex-width' => false,
+          'flex-height' => false,
+        )
+      )
+    );
+  }
 
-	/**
-	 * Replace CSS class of the logo
-	 * @param $html
-	 * @return array|string|string[]
-	 */
-	public function change_logo_class( $html ) {
+  /**
+   * Replace CSS class of the logo
+   * @param $html
+   * @return array|string|string[]
+   */
+  public function change_logo_class($html): array|string
+  {
 
-		$html = str_replace( 'custom-logo-link', 'logo', $html );
-		$html = str_replace( 'custom-logo', 'logo__image', $html );
+    $html = str_replace('custom-logo-link', 'logo', $html);
+    $html = str_replace('custom-logo', 'logo__image', $html);
 
 
-		return $html;
-	}
+    return $html;
+  }
 }
